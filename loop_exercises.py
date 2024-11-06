99a2440542bbf03882321374ca1c09c2d3a49ab8
kris-v <kristjan.kraak@voco.ee>
2024-11-06 14:10:06 +0200

    Create loop_exercises.py

---
+++ loop_exercises.py
@@ -0,0 +1,145 @@
+"""Loop exercises."""
+
+
+def sum_between(start: int, end: int) -> int:
+    """
+    Return sum of integers between start and end (both included).
+
+    print(sum_between(3, 5)) => 3 + 4 + 5 = 12
+    print(sum_between(5, 5)) => 5
+    """
+    sum_between = 0
+    for i in range(start, end + 1):
+        sum_between += i
+    return sum_between
+
+
+def sum_of_even_numbers(n: int) -> int:
+    """
+    Return sum of even numbers from 0 up to n (included).
+
+    print(sum_of_even_numbers(5)) => 0 + 2 + 4 = 6
+    print(sum_of_even_numbers(0)) => 0
+    """
+    sum_of_even_numbers = 0
+    for i in range(0, n + 1):
+        if i % 2 == 0:
+            sum_of_even_numbers += i
+    return sum_of_even_numbers
+
+
+def sum_of_multiples(n: int, end: int) -> int:
+    """
+    Return sum of numbers which are multiples of n between 0 and end (included).
+
+    print(sum_of_multiples(3, 10)) => 3 + 6 + 9 = 18
+    print(sum_of_multiples(7, 10)) => 7
+    print(sum_of_multiples(11, 10)) => 0
+    """
+    sum_of_multiples = 0
+    for i in range(n, end + 1, n):
+        sum_of_multiples += i
+    return sum_of_multiples
+    
+
+
+def cross_sum(numbers: str) -> int:
+    """
+    Return cross sum of numbers.
+
+    print(cross_sum("1234")) => 1 + 2 + 3 + 4 = 10
+    print(cross_sum("0")) => 0
+    print(cross_sum("4129458")) => 33
+    """
+    cross_sum = 0
+    for i in numbers:
+        cross_sum += int(i)
+    return cross_sum
+
+
+def multiply_between(start: int, end: int) -> int:
+    """
+    Multiply all numbers between start and end (both included).
+
+    print(multiply_between(1, 3)) => 1 * 2 * 3 = 6
+    print(multiply_between(4, 14)) => 14529715200
+    print(multiply_between(0, 7)) => 0
+    """
+    multiply_between = 1
+    for i in range(start, end + 1):
+        multiply_between = multiply_between * i
+    return multiply_between
+
+
+def make_hola_string(count: int) -> str:
+    """
+    Make hola string.
+
+    print(make_hola_string(3)) => "holaholahola"
+    print(make_hola_string(0)) => ""
+    """
+    make_hola_string = ""
+    for i in range(count):
+        make_hola_string += "hola"
+    return make_hola_string
+    
+
+
+def compound_interest(amount: int, years: int, rate: int) -> float:
+    """
+    Calculate compound interest.
+
+    print(compound_interest(100, 2, 2)) => 104.04
+    print(compound_interest(2000, 6, 8)) => 3173.748645888
+    """
+    Amount = amount
+    for i in range(years):
+        Amount = Amount * (1 + rate/100)
+    compound_interest = Amount
+    return compound_interest
+    
+
+def remove_vowels(original_string: str) -> str:
+    """
+    Return the given string without vowels.
+
+    print(remove_vowels("tere-tere")) => tr-tr
+    print(remove_vowels("hklmn")) => hklmn
+    print(remove_vowels("aauuiii")) => ""
+    """
+    remove_vowels = ""
+    vowels = ["a", "e", "i", "o", "u", "õ", "ä", "ö", "ü"]
+    for letter in original_string:
+        if letter not in vowels:
+            remove_vowels = remove_vowels + letter
+    return remove_vowels
+
+
+if __name__ == '__main__':
+    print(sum_between(3, 5))  # => 3 + 4 + 5 = 12
+    print(sum_between(5, 5))  # => 5
+    
+    print(sum_of_even_numbers(5))  # => 0 + 2 + 4 = 6
+    print(sum_of_even_numbers(0))  # => 0
+
+    print(sum_of_multiples(3, 10))  # => 3 + 6 + 9 = 18
+    print(sum_of_multiples(7, 10))  # => 7
+    print(sum_of_multiples(11, 10))  # => 0
+
+    print(cross_sum("1234"))  # => 1 + 2 + 3 + 4 = 10
+    print(cross_sum("0"))  # => 0
+    print(cross_sum("4129458"))  # => 33
+
+    print(multiply_between(1, 3))  # => 1 * 2 * 3 = 6
+    print(multiply_between(4, 14))  # => 14529715200
+    print(multiply_between(0, 7))  # => 0
+
+    print(make_hola_string(3))  # => "holaholahola"
+    print(make_hola_string(0))  # => ""
+
+    print(compound_interest(100, 2, 2))  # => 104.04
+    print(compound_interest(2000, 6, 8))  # => 3173.748645888
+    
+    print(remove_vowels("tere-tere"))  # => tr-tr
+    print(remove_vowels("hklmn"))  # => hklmn
+    print(remove_vowels("aauuiii"))  # => ""
