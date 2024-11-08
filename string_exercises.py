ba2198d45c39e1cce313ef64b954b9b4e33ef607
kris-v <kristjan.kraak@voco.ee>
2024-11-08 15:04:31 +0200

    Create string_exercises.py

---
+++ string_exercises.py
@@ -0,0 +1,21 @@
+# 1
+
+first_name = "James"
+last_name = "Bond"
+
+# 2
+
+full_name = first_name + " " + last_name
+self_description_sentence = "My name is " + last_name + ", " + first_name + " " + last_name + "."
+
+# 3
+
+cake = "vahukoormarjadtäidispõhi"
+print("vahukoor\nmarjad\ntäidis\npõhi")
+
+# 4
+
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = "Programming"[::-1]
